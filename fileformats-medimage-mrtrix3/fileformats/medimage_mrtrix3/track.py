87153b2ba912270e4e47465f4f73011ba7a2325c
adsouza <arkievdsouza@sydney.edu.au>
2024-03-28 10:22:46 +1100

    final modifications to .py files

--- fileformats-medimage-mrtrix3/fileformats/medimage_mrtrix3/track.py
+++ fileformats-medimage-mrtrix3/fileformats/medimage_mrtrix3/track.py
@@ -5,5 +5,5 @@ from fileformats.core.mixin import WithMagicNumber
 class Tracks(WithMagicNumber, File):
 
     ext = ".tck"
-    magic_number = b"mrtrix tracks\n"
+    magic_number = b"mrtrix tracks"
     binary = True
